800818517016122c2fb1e9324feea133507c1af6
TrvMarco <trevisanello.marco@gmail.com>
2022-04-28 14:45:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// 1. Chiedere ad utente numero di chilometri da percorrere
+// 2. Chiedere all'utente l'età 
+// 3. Calcolare il prezzo totale del viaggio
+// 4. Scontare il biglietto in base all'età del passeggero e mostrare risultato
